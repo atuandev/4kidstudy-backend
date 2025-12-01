9a37830f83a26ad0a83d7aeb5ede000c08adb485
atuandev <atuandev@gmail.com>
2025-12-01 20:54:09 +0700

    feat: add statistic most wrong exercise

---
+++ src/modules/attempt/dto/res/most-wrong-exercise-response.dto.ts
@@ -0,0 +1,13 @@
+/**
+ * Response DTO for most wrong exercises in a lesson
+ */
+export class MostWrongExerciseStatsDto {
+  exerciseId: number;
+  exerciseOrder: number;
+  exercisePrompt?: string;
+  firstWrongCount: number;
+  secondWrongCount: number;
+  totalWrongCount: number;
+}
+
+
